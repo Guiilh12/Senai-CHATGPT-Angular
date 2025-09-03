aee19dc89d7d4c9e910e54023b3b9b67fca47531
FIC <fic@info.local>
2025-09-03 19:56:27 -0300

    senai-web-angular

---
+++ src/app/app.routes.ts
@@ -0,0 +1,8 @@
+import { Routes } from '@angular/router';
+import { LoginScreenComponent } from './user-module/login-screen/login-screen.component';
+
+export const routes: Routes = [{
+    path:"login",
+    loadComponent:()=> LoginScreenComponent
+    }
+];
