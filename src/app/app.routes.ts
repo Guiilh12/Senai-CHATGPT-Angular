2f82e8482e226beaabd2d9cb2cacafbc826c12b4
FIC <fic@info.local>
2025-09-09 21:52:58 -0300

    cadastro

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { LoginScreenComponent } from './user-module/login-screen/login-screen.component';
 import { ChatScreenComponent } from './chat-screen/chat-screen.component';
 import { authGuard } from '../auth.guard';
+import { NewUserScreenComponent } from './user-module/new-user-screen/new-user-screen.component';
 
 export const routes: Routes = [{
     path:"login",
@@ -17,5 +18,8 @@ export const routes: Routes = [{
     path:"chat",
     loadComponent: () => ChatScreenComponent,
     canActivate: [authGuard]
-  }
+  }, 
+  {path:"new-user",
+  loadComponent: () => NewUserScreenComponent
+}
 ] 
